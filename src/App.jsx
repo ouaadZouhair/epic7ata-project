14f7f7c3f672c01f1815153111523483b76b4a4f
ouaadZouhair <ouaadzouhair11@gmail.com>
2025-01-07 16:03:48 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./components/Navbar/Navbar.jsx"
+import TopNav from "./components/Navbar/TopNav.jsx"
+
+function App() {
+
+  return (
+    <>
+      <TopNav/>
+      <Navbar/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar/TopNav.jsx
@@ -0,0 +1,9 @@
+const TopNav = () =>{
+    return(
+        <div className="bg-gray-950 w-full h-14 flex justify-around">
+
+        </div>
+    );
+}
+
+export default TopNav;
